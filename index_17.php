28ee51fb6e1f3a1e17a5912223e1a6caf61d0dc0
Derrnux <45166410+Derrnux@users.noreply.github.com>
2020-02-05 15:44:05 +0100

    progressbar fix

--- index_17.php
+++ index_17.php
@@ -59,7 +59,7 @@ Wo sind die, die vor uns auf der Welt waren? Geht in die obere Welt, geht in die
 	       <div class="header">
 	            <div class="h1" style="">Social Media Umfrage 2020 [Teil 2]</div>
 	            <div class="progress" style="">
-                    <div class="progress-bar" role="progressbar" style="width: 5%" aria-valuenow="95" aria-valuemin="0" aria-valuemax="100"></div>
+                    <div class="progress-bar" role="progressbar" style="width: 95%" aria-valuenow="95" aria-valuemin="0" aria-valuemax="100"></div>
                 </div>
                 <div class="frage">
                     <div class="h2" style="">Ich fühle mich oft alleine</div>
