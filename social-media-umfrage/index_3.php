162ef69d48895f88d5f71e0639719a2e073a9814
Derrnux <45166410+Derrnux@users.noreply.github.com>
2020-02-05 15:31:47 +0100

    progressbar fix

--- social-media-umfrage/index_3.php
+++ social-media-umfrage/index_3.php
@@ -58,7 +58,7 @@ Wo sind die, die vor uns auf der Welt waren? Geht in die obere Welt, geht in die
 	       <div class="header">
 	            <div class="h1" style="">Social Media Umfrage 2020</div>
 	            <div class="progress" style="">
-                    <div class="progress-bar" role="progressbar" style="width: 5%" aria-valuenow="11" aria-valuemin="0" aria-valuemax="100"></div>
+                    <div class="progress-bar" role="progressbar" style="width: 11%" aria-valuenow="11" aria-valuemin="0" aria-valuemax="100"></div>
                 </div>
                 <div class="frage">
                     <div class="h2" style="">Welchen Beruf &uuml;bst du aus?</div>
