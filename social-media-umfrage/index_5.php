2c13b38c2c5c4085321f113d7b87afde7422d2d2
Derrnux <45166410+Derrnux@users.noreply.github.com>
2020-02-09 22:26:49 +0100

    neue sortierung

---
+++ social-media-umfrage/index_5.php
@@ -0,0 +1,135 @@
+<?php
+$check1 = $_POST['Lieblingsfach'];
+
+$ip = $_SERVER['REMOTE_ADDR'];
+$link = mysqli_connect (
+                     'smu2020.ddns.net',
+                     'phppage',
+					 'R2*jz24oNU@.y8',
+					 'umfrage'
+                   );
+
+$sql = "UPDATE fragen SET index4='$check1' WHERE id='$ip'";
+$result = mysqli_query($link,$sql) or die("Anfrage fehlgeschlagen: " . mysqli_error($link));
+?>
+
+<!DOCTYPE html>
+<html lang="de">
+
+	<head>
+		<meta charset="UTF-8">
+		<title>Social Media Umfrage</title>
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous" />
+		<link rel="stylesheet" href="style.css" />
+	</head>
+	
+	<body>
+
+<!-- Modal 3!!2 -->
+<div class="modal fade" id="modal32" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-scrollable" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle" style="">Disclaimer</h5>
+      </div>
+      <div class="modal-body">
+          <b>Grundlegendes</b>
+
+</br>Diese Datenschutzerklärung soll die Nutzer dieser Website über die Art, den Umfang und den Zweck der Erhebung und Verwendung personenbezogener Daten durch den Websitebetreiber [Informatik Lk Schülergruppe des Gymnasiums Stift Keppel] informieren.
+</br>Der Websitebetreiber nimmt Ihren Datenschutz sehr ernst und behandelt Ihre personenbezogenen Daten vertraulich und entsprechend der gesetzlichen Vorschriften. Da durch neue Technologien und die ständige Weiterentwicklung dieser Webseite Änderungen an dieser Datenschutzerklärung vorgenommen werden können, empfehlen wir Ihnen sich die Datenschutzerklärung in regelmäßigen Abständen wieder durchzulesen.
+</br>Definitionen der verwendeten Begriffe (z.B. “personenbezogene Daten” oder “Verarbeitung”) finden Sie in Art. 4 DSGVO.
+</br></br>
+<b>Zugriffsdaten</b>
+</br>
+Wir, der Websitebetreiber bzw. Seitenprovider, erheben aufgrund unseres berechtigten Interesses (s. Art. 6 Abs. 1 lit. f. DSGVO) Daten über Zugriffe auf die Website und speichern diese als “Server-Logfiles” auf dem Server der Website ab. Folgende Daten werden so protokolliert:
+<ol><li>Besuchte Website
+    <li>Verwendeter Browser
+    <li>Verwendetes Betriebssystem
+    <li>Verwendete IP-Adresse</ol>
+</br>Die Server-Logfiles werden für maximal 7 Tage gespeichert und anschließend gelöscht. Die Speicherung der Daten erfolgt aus Sicherheitsgründen, um z. B. Missbrauchsfälle aufklären zu können. Müssen Daten aus Beweisgründen aufgehoben werden, sind sie solange von der Löschung ausgenommen bis der Vorfall endgültig geklärt ist.
+</br></br>
+<b>Erfassung und Verarbeitung personenbezogener Daten</b>
+</br>Der Websitebetreiber erhebt, nutzt und gibt Ihre personenbezogenen Daten nur dann weiter, wenn dies im gesetzlichen Rahmen erlaubt ist oder Sie in die Datenerhebung einwilligen.
+</br>Als personenbezogene Daten gelten sämtliche Informationen, welche dazu dienen, Ihre Person zu bestimmen und welche zu Ihnen zurückverfolgt werden können – also beispielsweise Ihr Name, Ihre E-Mail-Adresse und Telefonnummer.
+</br>Diese Website können Sie auch besuchen, ohne Angaben zu Ihrer Person zu machen. Zur Verbesserung unseres Online-Angebotes speichern wir jedoch (ohne Personenbezug) Ihre Zugriffsdaten auf diese Website. Zu diesen Zugriffsdaten gehören z. B. die von Ihnen angeforderte Datei oder der Name Ihres Internet-Providers. Durch die Anonymisierung der Daten sind Rückschlüsse auf Ihre Person nicht möglich.
+</br>Diese Daten werden erhoben und gespeichert bei der beantwortung und Teilnahme an unserer Umfrage.
+
+</br></br>
+<b>Rechte des Nutzers</b>
+
+</br>Sie haben als Nutzer das Recht, auf Antrag eine kostenlose Auskunft darüber zu erhalten, welche personenbezogenen Daten über Sie gespeichert wurden. Sie haben außerdem das Recht auf Berichtigung falscher Daten und auf die Verarbeitungseinschränkung oder Löschung Ihrer personenbezogenen Daten. Falls zutreffend, können Sie auch Ihr Recht auf Datenportabilität geltend machen. Sollten Sie annehmen, dass Ihre Daten unrechtmäßig verarbeitet wurden, können Sie eine Beschwerde bei der zuständigen Aufsichtsbehörde einreichen.
+</br>Löschung von Daten
+</br>Sofern Ihr Wunsch nicht mit einer gesetzlichen Pflicht zur Aufbewahrung von Daten (z. B. Vorratsdatenspeicherung) kollidiert, haben Sie ein Anrecht auf Löschung Ihrer Daten. Von uns gespeicherte Daten werden, sollten sie für ihre Zweckbestimmung nicht mehr vonnöten sein und es keine gesetzlichen Aufbewahrungsfristen geben, gelöscht. Falls eine Löschung nicht durchgeführt werden kann, da die Daten für zulässige gesetzliche Zwecke erforderlich sind, erfolgt eine Einschränkung der Datenverarbeitung. In diesem Fall werden die Daten gesperrt und nicht für andere Zwecke verarbeitet.
+</br>Widerspruchsrecht
+</br>Nutzer dieser Webseite können von ihrem Widerspruchsrecht Gebrauch machen und der Verarbeitung ihrer personenbezogenen Daten zu jeder Zeit widersprechen.
+</br>Wenn Sie eine Berichtigung, Sperrung, Löschung oder Auskunft über die zu Ihrer Person gespeicherten personenbezogenen Daten wünschen oder Fragen bzgl. der Erhebung, Verarbeitung oder Verwendung Ihrer personenbezogenen Daten haben oder erteilte Einwilligungen widerrufen möchten, wenden Sie sich bitte an folgende  <font color="red">E-Mail-Adresse: [muster@stiftkeppel.de]</font>
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-outline-secondary" data-dismiss="modal" style="margin-right:0px;">Schlie&szlig;en</button>
+      </div>
+    </div>
+  </div>
+</div>
+	
+	   <div class="wrapper">
+	       <div class="header">
+	            <div class="h1" style="">Social Media Umfrage 2020</div>
+	            <div class="progress" style="">
+                    <div class="progress-bar" role="progressbar" style="width: 22.21%" aria-valuenow="22.21" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+                <div class="frage">
+                    <div class="h2" style="">Welche dieser Social Media App's verwendest du? (Mehrfachnennung möglich)</div>
+                    <form method="post" action="index_6.php">
+                        <div class="form-check" style="">         
+                            <input name="checkbox1" value="0" type="hidden" /> 	<!--Wird genutzt damit die Checkboxen auch "null" sein können -->
+                            <input name="checkbox2" value="0" type="hidden" />
+                            <input name="checkbox3" value="0" type="hidden" />
+							<input name="checkbox4" value="0" type="hidden" />
+							<input name="checkbox5" value="0" type="hidden" />
+							<input name="checkbox6" value="0" type="hidden" />
+                            
+                            <!-- 1 -->
+                            <input type="checkbox" name="checkbox1" class="form-check-input" id="checkbox1" value="1" />
+                            <label class="form-check-label" for="SMApp1" style="margin-bottom: 4px;">Instagram</label><br />
+							<!-- 2 -->
+                            <input type="checkbox" class="form-check-input" id="checkbox2" name="checkbox2" value="1" />
+                            <label class="form-check-label" for="SMApp2" style="margin-bottom: 4px;">YouTube</label><br />
+							<!-- 3 -->
+                            <input type="checkbox" class="form-check-input" id="checkbox3" name="checkbox3" value="1"/>
+                            <label class="form-check-label" for="SMApp3" style="margin-bottom: 4px;">Twitter</label><br />
+							<!-- 4 -->
+                            <input type="checkbox" name="checkbox4" class="form-check-input" id="checkbox4" value="1" />
+                            <label class="form-check-label" for="SMApp4" style="margin-bottom: 4px;">Facebook</label><br />
+							<!-- 5 -->
+                            <input type="checkbox" class="form-check-input" id="checkbox5" name="checkbox5" value="1" />
+                            <label class="form-check-label" for="SMApp5" style="margin-bottom: 4px;">TikTok</label><br />
+							<!-- 6 -->
+                            <input type="checkbox" class="form-check-input" id="checkbox6" name="checkbox6" value="1"/>
+                            <label class="form-check-label" for="SMApp6" style="margin-bottom: 4px;">Pinterest</label><br />
+                        </div>
+                        <div class="center" style="">
+                        <a href="index_4.php"><button type="button" class="btn btn-outline-secondary" style="" >Zur&uuml;ck</button></a>
+                        <button type="submit" class="btn btn-outline-primary" >Weiter</button>
+                        </div>
+                    </form>
+                </div>
+                <div class="footer">
+                <a data-toggle="modal" href="#modal32">Disclaimer</a>
+                </div>
+           </div>
+       </div>
+       
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+<div class="alerto" role="alert" style="">
+<?php
+
+?>
+</div>
+
+    </body>
+    
+</html>
