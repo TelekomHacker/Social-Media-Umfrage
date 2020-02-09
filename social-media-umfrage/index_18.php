98d422243ec299811b713c6f0f944b3cba101c97
Derrnux <45166410+Derrnux@users.noreply.github.com>
2020-02-09 17:42:32 +0100

    progressbar

--- social-media-umfrage/index_18.php
+++ social-media-umfrage/index_18.php
@@ -77,7 +77,7 @@ Wir, der Websitebetreiber bzw. Seitenprovider, erheben aufgrund unseres berechti
 	       <div class="header">
 	            <div class="h1" style="">Social Media Umfrage 2020 [Teil 2]</div>
 	            <div class="progress" style="">
-                    <div class="progress-bar" role="progressbar" style="width: 95%" aria-valuenow="95" aria-valuemin="0" aria-valuemax="100"></div>
+                    <div class="progress-bar" role="progressbar" style="width: 94.38%" aria-valuenow="94.38" aria-valuemin="0" aria-valuemax="100"></div>
                 </div>
                 <div class="frage">
                     <div class="h2" style="">Ich fühle mich oft alleine</div>
