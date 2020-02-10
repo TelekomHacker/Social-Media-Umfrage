eedba9f317c0e1d2fdb05608f4a7e1056bb3f033
Derrnux <45166410+Derrnux@users.noreply.github.com>
2020-02-10 03:36:04 +0100

    Update index_17.php

--- social-media-umfrage/index_17.php
+++ social-media-umfrage/index_17.php
@@ -80,7 +80,7 @@ Wir, der Websitebetreiber bzw. Seitenprovider, erheben aufgrund unseres berechti
                 </div>
                 <div class="frage">
                     <div class="h2" style="">Ich werde nicht oft eingeladen etwas mit anderen zu unternehmen</div>
-                    <form method="post" action="index_12.php">
+                    <form method="post" action="index_18.php">
                         <div class="form-check" style="">         
                             
 				
@@ -97,7 +97,7 @@ Wir, der Websitebetreiber bzw. Seitenprovider, erheben aufgrund unseres berechti
 			    
                         </div>
                         <div class="center" style="">
-                        <a href="index_10.php"><button type="button" class="btn btn-outline-secondary" style="" >Zur&uuml;ck</button></a>
+                        <a href="index_16.php"><button type="button" class="btn btn-outline-secondary" style="" >Zur&uuml;ck</button></a>
                         <button type="submit" class="btn btn-outline-primary" >Weiter</button>
                         </div>
                     </form>
