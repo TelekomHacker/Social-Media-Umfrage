850a87561e8e5e5d1fe50a2b32498754e43bfe05
nils137 <60094415+nils137@users.noreply.github.com>
2020-02-10 11:21:46 +0100

    Datenbank

--- umfrage.sql
+++ umfrage.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.1
+-- version 4.9.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 29. Jan 2020 um 11:31
--- Server-Version: 10.4.8-MariaDB
--- PHP-Version: 7.3.10
+-- Generation Time: Feb 10, 2020 at 11:21 AM
+-- Server version: 10.4.10-MariaDB
+-- PHP Version: 7.3.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,16 +19,44 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Datenbank: `umfrage`
+-- Database: `umfrage`
 --
 
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `messengerdienste`
+-- Table structure for table `fragen`
+--
+
+CREATE TABLE `fragen` (
+  `id` int(20) NOT NULL,
+  `index1` varchar(20) NOT NULL,
+  `index2` varchar(20) NOT NULL,
+  `index3` text NOT NULL,
+  `index4` text NOT NULL,
+  `index5` int(11) NOT NULL COMMENT 'Fremdschlüssel',
+  `index6` int(11) NOT NULL COMMENT 'Fremdschlüssel',
+  `index7` int(11) NOT NULL COMMENT 'Fremdschlüssel',
+  `index8` varchar(20) NOT NULL,
+  `index9` int(1) NOT NULL,
+  `index10` int(1) NOT NULL,
+  `index11` int(1) NOT NULL,
+  `index12` int(1) NOT NULL,
+  `index13` int(1) NOT NULL,
+  `index14` int(1) NOT NULL,
+  `index15` int(1) NOT NULL,
+  `index16` int(1) NOT NULL,
+  `index17` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `messengerdienste`
 --
 
 CREATE TABLE `messengerdienste` (
+  `m_ip` text COLLATE latin1_german1_ci NOT NULL,
   `MD_ID` int(11) NOT NULL,
   `Whatsapp` tinyint(1) NOT NULL,
   `Snapchat` tinyint(1) NOT NULL,
@@ -37,30 +65,57 @@ CREATE TABLE `messengerdienste` (
   `Fbmessenger` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_german1_ci;
 
+--
+-- Dumping data for table `messengerdienste`
+--
+
+INSERT INTO `messengerdienste` (`m_ip`, `MD_ID`, `Whatsapp`, `Snapchat`, `Telegram`, `Threema`, `Fbmessenger`) VALUES
+('::1', 7, 1, 1, 0, 0, 0),
+('::1', 8, 0, 0, 0, 0, 0),
+('::1', 9, 0, 0, 0, 0, 0),
+('::1', 10, 1, 1, 0, 0, 0),
+('::1', 11, 0, 0, 0, 0, 0),
+('::1', 12, 1, 1, 0, 0, 0),
+('::1', 13, 1, 1, 0, 0, 0),
+('::1', 14, 1, 1, 0, 0, 0);
+
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `person`
+-- Table structure for table `nutzungsdauer`
 --
 
-CREATE TABLE `person` (
-  `P_ID` int(11) NOT NULL,
-  `Altersgruppe` smallint(6) NOT NULL,
-  `Geschlecht` tinytext COLLATE latin1_german1_ci NOT NULL,
-  `Beruf` tinytext COLLATE latin1_german1_ci NOT NULL,
-  `Lieblingsfach` tinytext COLLATE latin1_german1_ci NOT NULL,
-  `SocialMediaApp` int(11) NOT NULL,
-  `MessengerDienste` int(11) NOT NULL,
-  `Nutzungsdauer` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_german1_ci;
+CREATE TABLE `nutzungsdauer` (
+  `n_ip` int(11) NOT NULL,
+  `ND_ID` int(11) NOT NULL,
+  `Instagram` smallint(6) DEFAULT NULL,
+  `YouTube` smallint(6) DEFAULT NULL,
+  `Twitter` smallint(6) DEFAULT NULL,
+  `Facebook` smallint(6) DEFAULT NULL,
+  `TikTok` smallint(6) DEFAULT NULL,
+  `Pinterest` smallint(6) DEFAULT NULL,
+  `WhatsApp` smallint(6) DEFAULT NULL,
+  `Snapchat` smallint(6) DEFAULT NULL,
+  `Telegram` smallint(6) DEFAULT NULL,
+  `Threema` smallint(6) DEFAULT NULL,
+  `FB_Messenger` smallint(6) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `nutzungsdauer`
+--
+
+INSERT INTO `nutzungsdauer` (`n_ip`, `ND_ID`, `Instagram`, `YouTube`, `Twitter`, `Facebook`, `TikTok`, `Pinterest`, `WhatsApp`, `Snapchat`, `Telegram`, `Threema`, `FB_Messenger`) VALUES
+(0, 1, 111, 222, 333, 0, 0, 0, 187, 187, 0, 0, 0);
 
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `socialmediaapps`
+-- Table structure for table `socialmediaapps`
 --
 
 CREATE TABLE `socialmediaapps` (
+  `s_ip` text COLLATE latin1_german1_ci NOT NULL,
   `SM_ID` int(11) NOT NULL,
   `Instagram` tinyint(1) NOT NULL,
   `Youtube` tinyint(1) NOT NULL,
@@ -71,48 +126,71 @@ CREATE TABLE `socialmediaapps` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_german1_ci;
 
 --
--- Indizes der exportierten Tabellen
+-- Dumping data for table `socialmediaapps`
+--
+
+INSERT INTO `socialmediaapps` (`s_ip`, `SM_ID`, `Instagram`, `Youtube`, `Twitter`, `Facebook`, `Tiktok`, `Pinterest`) VALUES
+('::1', 7, 1, 1, 1, 0, 0, 0),
+('::1', 8, 1, 1, 1, 0, 0, 0),
+('::1', 9, 1, 1, 1, 0, 0, 0),
+('::1', 10, 1, 1, 1, 0, 0, 0),
+('::1', 11, 1, 1, 1, 0, 0, 0);
+
+--
+-- Indexes for dumped tables
 --
 
 --
--- Indizes für die Tabelle `messengerdienste`
+-- Indexes for table `fragen`
+--
+ALTER TABLE `fragen`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `messengerdienste`
 --
 ALTER TABLE `messengerdienste`
   ADD PRIMARY KEY (`MD_ID`);
 
 --
--- Indizes für die Tabelle `person`
+-- Indexes for table `nutzungsdauer`
 --
-ALTER TABLE `person`
-  ADD PRIMARY KEY (`P_ID`);
+ALTER TABLE `nutzungsdauer`
+  ADD PRIMARY KEY (`ND_ID`);
 
 --
--- Indizes für die Tabelle `socialmediaapps`
+-- Indexes for table `socialmediaapps`
 --
 ALTER TABLE `socialmediaapps`
   ADD PRIMARY KEY (`SM_ID`);
 
 --
--- AUTO_INCREMENT für exportierte Tabellen
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT für Tabelle `messengerdienste`
+-- AUTO_INCREMENT for table `fragen`
+--
+ALTER TABLE `fragen`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `messengerdienste`
 --
 ALTER TABLE `messengerdienste`
-  MODIFY `MD_ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `MD_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
--- AUTO_INCREMENT für Tabelle `person`
+-- AUTO_INCREMENT for table `nutzungsdauer`
 --
-ALTER TABLE `person`
-  MODIFY `P_ID` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `nutzungsdauer`
+  MODIFY `ND_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT für Tabelle `socialmediaapps`
+-- AUTO_INCREMENT for table `socialmediaapps`
 --
 ALTER TABLE `socialmediaapps`
-  MODIFY `SM_ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `SM_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
