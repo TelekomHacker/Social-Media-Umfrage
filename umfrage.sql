68bbf1414ca40d2d22243a01104118b3c380cdbe
nils137 <60094415+nils137@users.noreply.github.com>
2020-02-10 14:02:34 +0100

    Doch ne ID in jeder Tabelle

--- umfrage.sql
+++ umfrage.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.1
+-- version 4.9.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 10. Feb 2020 um 13:02
--- Server-Version: 10.4.11-MariaDB
--- PHP-Version: 7.4.2
+-- Generation Time: Feb 10, 2020 at 02:02 PM
+-- Server version: 10.4.8-MariaDB
+-- PHP Version: 7.3.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,17 +19,18 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Datenbank: `umfrage`
+-- Database: `umfrage`
 --
 
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `fragen`
+-- Table structure for table `fragen`
 --
 
 CREATE TABLE `fragen` (
   `id` int(20) NOT NULL,
+  `F_ID` int(11) NOT NULL,
   `index1` int(1) NOT NULL,
   `index2` int(1) NOT NULL,
   `index3` int(1) NOT NULL,
@@ -62,11 +63,12 @@ CREATE TABLE `fragen` (
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `messengerdienste`
+-- Table structure for table `messengerdienste`
 --
 
 CREATE TABLE `messengerdienste` (
   `m_ip` int(20) NOT NULL,
+  `M_ID` int(11) NOT NULL,
   `Whatsapp` tinyint(1) NOT NULL,
   `Snapchat` tinyint(1) NOT NULL,
   `Telegram` tinyint(1) NOT NULL,
@@ -77,11 +79,12 @@ CREATE TABLE `messengerdienste` (
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `nutzungsdauer`
+-- Table structure for table `nutzungsdauer`
 --
 
 CREATE TABLE `nutzungsdauer` (
   `n_ip` int(20) NOT NULL,
+  `ND_ID` int(11) NOT NULL,
   `Instagram` smallint(6) DEFAULT NULL,
   `YouTube` smallint(6) DEFAULT NULL,
   `Twitter` smallint(6) DEFAULT NULL,
@@ -98,11 +101,12 @@ CREATE TABLE `nutzungsdauer` (
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `nutzungsgruende`
+-- Table structure for table `nutzungsgruende`
 --
 
 CREATE TABLE `nutzungsgruende` (
   `nt_ip` int(20) NOT NULL,
+  `NG_ID` int(11) NOT NULL,
   `Memes` int(1) NOT NULL,
   `Beruflich` int(1) NOT NULL,
   `Prominente` int(1) NOT NULL,
@@ -114,11 +118,12 @@ CREATE TABLE `nutzungsgruende` (
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `socialmediaapps`
+-- Table structure for table `socialmediaapps`
 --
 
 CREATE TABLE `socialmediaapps` (
   `s_ip` int(20) NOT NULL,
+  `S_ID` int(11) NOT NULL,
   `Instagram` tinyint(1) NOT NULL,
   `Youtube` tinyint(1) NOT NULL,
   `Twitter` tinyint(1) NOT NULL,
@@ -128,38 +133,72 @@ CREATE TABLE `socialmediaapps` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_german1_ci;
 
 --
--- Indizes der exportierten Tabellen
+-- Indexes for dumped tables
 --
 
 --
--- Indizes für die Tabelle `fragen`
+-- Indexes for table `fragen`
 --
 ALTER TABLE `fragen`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`F_ID`);
 
 --
--- Indizes für die Tabelle `messengerdienste`
+-- Indexes for table `messengerdienste`
 --
 ALTER TABLE `messengerdienste`
-  ADD PRIMARY KEY (`m_ip`);
+  ADD PRIMARY KEY (`M_ID`);
 
 --
--- Indizes für die Tabelle `nutzungsdauer`
+-- Indexes for table `nutzungsdauer`
 --
 ALTER TABLE `nutzungsdauer`
-  ADD PRIMARY KEY (`n_ip`);
+  ADD PRIMARY KEY (`ND_ID`);
 
 --
--- Indizes für die Tabelle `nutzungsgruende`
+-- Indexes for table `nutzungsgruende`
 --
 ALTER TABLE `nutzungsgruende`
-  ADD PRIMARY KEY (`nt_ip`);
+  ADD PRIMARY KEY (`NG_ID`);
 
 --
--- Indizes für die Tabelle `socialmediaapps`
+-- Indexes for table `socialmediaapps`
 --
 ALTER TABLE `socialmediaapps`
-  ADD PRIMARY KEY (`s_ip`);
+  ADD PRIMARY KEY (`S_ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `fragen`
+--
+ALTER TABLE `fragen`
+  MODIFY `F_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `messengerdienste`
+--
+ALTER TABLE `messengerdienste`
+  MODIFY `M_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `nutzungsdauer`
+--
+ALTER TABLE `nutzungsdauer`
+  MODIFY `ND_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `nutzungsgruende`
+--
+ALTER TABLE `nutzungsgruende`
+  MODIFY `NG_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `socialmediaapps`
+--
+ALTER TABLE `socialmediaapps`
+  MODIFY `S_ID` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
